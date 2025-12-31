2f91c5535ed7acd8a0dd15c4400ad970c862cd63
Derese4803 <derese.esubalew-c@oneacrefund.org>
2025-12-31 11:58:24 +0300

    Update models.py
    Signed-off-by: Derese4803 <derese.esubalew-c@oneacrefund.org>

--- models.py
+++ models.py
@@ -1,28 +1,56 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey, DateTime
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 import datetime
 
 Base = declarative_base()
+
+class Woreda(Base):
+    __tablename__ = 'woredas'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    kebeles = relationship("Kebele", backref="woreda")
+
+class Kebele(Base):
+    __tablename__ = 'kebeles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    woreda_id = Column(Integer, ForeignKey('woredas.id'))
+
+class Farmer(Base):
+    __tablename__ = 'farmers'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    phone = Column(String)
+    woreda = Column(String)
+    kebele = Column(String)
+    officer_name = Column(String)
+    audio_url = Column(String)
+    # Seedlings
+    gesho = Column(Integer, default=0)
+    giravila = Column(Integer, default=0)
+    diceres = Column(Integer, default=0)
+    wanza = Column(Integer, default=0)
+    papaya = Column(Integer, default=0)
+    moringa = Column(Integer, default=0)
+    lemon = Column(Integer, default=0)
+    arzelibanos = Column(Integer, default=0)
+    guava = Column(Integer, default=0)
 
 class BackCheck(Base):
     __tablename__ = 'back_checks'
     id = Column(Integer, primary_key=True)
     checker_name = Column(String)
     fenced = Column(String)
-    # Guava
     guava_beds = Column(Integer)
     guava_length = Column(Float)
     guava_sockets = Column(Integer)
-    # Lemon
     lemon_beds = Column(Integer)
     lemon_length = Column(Float)
     lemon_sockets = Column(Integer)
-    # Gesho
     gesho_beds = Column(Integer)
     gesho_length = Column(Float)
     gesho_sockets = Column(Integer)
-    # Grevillea
     grevillea_beds = Column(Integer)
     grevillea_length = Column(Float)
     grevillea_sockets = Column(Integer)
