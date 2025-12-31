87da1f8d0dd8c8f8d7bd0bc3fc05baadcfe086d4
Derese4803 <derese.esubalew-c@oneacrefund.org>
2025-12-31 12:01:14 +0300

    Update models.py
    Signed-off-by: Derese4803 <derese.esubalew-c@oneacrefund.org>

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class Woreda(Base):
     __tablename__ = 'woredas'
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
-    kebeles = relationship("Kebele", backref="woreda")
+    kebeles = relationship("Kebele", backref="woreda", cascade="all, delete-orphan")
 
 class Kebele(Base):
     __tablename__ = 'kebeles'
@@ -26,6 +26,7 @@ class Farmer(Base):
     kebele = Column(String)
     officer_name = Column(String)
     audio_url = Column(String)
+    # Seedling Counts
     gesho = Column(Integer, default=0)
     giravila = Column(Integer, default=0)
     diceres = Column(Integer, default=0)
@@ -35,21 +36,26 @@ class Farmer(Base):
     lemon = Column(Integer, default=0)
     arzelibanos = Column(Integer, default=0)
     guava = Column(Integer, default=0)
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
 
 class BackCheck(Base):
     __tablename__ = 'back_checks'
     id = Column(Integer, primary_key=True)
     checker_name = Column(String)
     fenced = Column(String)
+    # Guava
     guava_beds = Column(Integer)
     guava_length = Column(Float)
     guava_sockets = Column(Integer)
+    # Lemon
     lemon_beds = Column(Integer)
     lemon_length = Column(Float)
     lemon_sockets = Column(Integer)
+    # Gesho
     gesho_beds = Column(Integer)
     gesho_length = Column(Float)
     gesho_sockets = Column(Integer)
+    # Grevillea
     grevillea_beds = Column(Integer)
     grevillea_length = Column(Float)
     grevillea_sockets = Column(Integer)
