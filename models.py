676411fe09b6436a774d37e30a1107763c6ebbb2
Derese4803 <derese.esubalew-c@oneacrefund.org>
2025-12-31 14:30:15 +0300

    Update models.py
    Signed-off-by: Derese4803 <derese.esubalew-c@oneacrefund.org>

--- models.py
+++ models.py
@@ -9,37 +9,19 @@ class BackCheck(Base):
     id = Column(Integer, primary_key=True)
     
     # Location & Personnel
-    woreda = Column(String)           # ወረዳ
-    cluster = Column(String)         # ክላስተር
-    kebele = Column(String)           # ቀበሌ
-    tno_name = Column(String)        # የTNO ስም
-    checker_fa_name = Column(String)  # የFA ስም
-    checker_cbe_name = Column(String) # የCBE ስም
-    checker_phone = Column(String)    # ስልክ ቁጥር
-    fenced = Column(String)           # አጥር አለው?
+    woreda = Column(String)
+    cluster = Column(String)
+    kebele = Column(String)
+    tno_name = Column(String)
+    checker_fa_name = Column(String)
+    checker_cbe_name = Column(String) 
+    checker_phone = Column(String)    
+    fenced = Column(String)           
 
-    # Guava (ዘይቶን)
-    guava_beds = Column(Integer)
-    guava_length = Column(Float)
-    guava_sockets = Column(Integer)
-    total_guava_sockets = Column(Integer)
-
-    # Gesho (ጌሾ)
-    gesho_beds = Column(Integer)
-    gesho_length = Column(Float)
-    gesho_sockets = Column(Integer)
-    total_gesho_sockets = Column(Integer)
-
-    # Lemon (ሎሚ)
-    lemon_beds = Column(Integer)
-    lemon_length = Column(Float)
-    lemon_sockets = Column(Integer)
-    total_lemon_sockets = Column(Integer)
-
-    # Grevillea (ግራቪሊያ)
-    grevillea_beds = Column(Integer)
-    grevillea_length = Column(Float)
-    grevillea_sockets = Column(Integer)
-    total_grevillea_sockets = Column(Integer) 
+    # Groups
+    guava_beds = Column(Integer); guava_length = Column(Float); guava_sockets = Column(Integer); total_guava_sockets = Column(Integer)
+    gesho_beds = Column(Integer); gesho_length = Column(Float); gesho_sockets = Column(Integer); total_gesho_sockets = Column(Integer)
+    lemon_beds = Column(Integer); lemon_length = Column(Float); lemon_sockets = Column(Integer); total_lemon_sockets = Column(Integer)
+    grevillea_beds = Column(Integer); grevillea_length = Column(Float); grevillea_sockets = Column(Integer); total_grevillea_sockets = Column(Integer) 
     
     timestamp = Column(DateTime, default=datetime.datetime.utcnow)
