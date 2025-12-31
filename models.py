b7448cbf866de86d3bc8e11318a048073fc66cdc
Derese4803 <derese.esubalew-c@oneacrefund.org>
2025-12-31 15:35:23 +0300

    Update models.py
    Signed-off-by: Derese4803 <derese.esubalew-c@oneacrefund.org>

--- models.py
+++ models.py
@@ -14,32 +14,20 @@ class BackCheck(Base):
     kebele = Column(String)
     tno_name = Column(String)
     checker_fa_name = Column(String)
-    cbe_acc = Column(String)          # This is the new field
+    cbe_acc = Column(String)          # This must match the keyword in app.py
     checker_phone = Column(String)    
     fenced = Column(String)           
 
     # Guava (ዘይቶን)
-    guava_beds = Column(Integer)
-    guava_length = Column(Float)
-    guava_sockets = Column(Integer)
-    total_guava_sockets = Column(Integer)
-
+    guava_beds = Column(Integer); guava_length = Column(Float); guava_sockets = Column(Integer); total_guava_sockets = Column(Integer)
+    
     # Gesho (ጌሾ)
-    gesho_beds = Column(Integer)
-    gesho_length = Column(Float)
-    gesho_sockets = Column(Integer)
-    total_gesho_sockets = Column(Integer)
+    gesho_beds = Column(Integer); gesho_length = Column(Float); gesho_sockets = Column(Integer); total_gesho_sockets = Column(Integer)
 
     # Lemon (ሎሚ)
-    lemon_beds = Column(Integer)
-    lemon_length = Column(Float)
-    lemon_sockets = Column(Integer)
-    total_lemon_sockets = Column(Integer)
+    lemon_beds = Column(Integer); lemon_length = Column(Float); lemon_sockets = Column(Integer); total_lemon_sockets = Column(Integer)
 
     # Grevillea (ግራቪሊያ)
-    grevillea_beds = Column(Integer)
-    grevillea_length = Column(Float)
-    grevillea_sockets = Column(Integer)
-    total_grevillea_sockets = Column(Integer) 
+    grevillea_beds = Column(Integer); grevillea_length = Column(Float); grevillea_sockets = Column(Integer); total_grevillea_sockets = Column(Integer) 
     
     timestamp = Column(DateTime, default=datetime.datetime.utcnow)
