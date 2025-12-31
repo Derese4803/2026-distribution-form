c5da5a4dc4735b41f82cf008fd21775a18418aa8
Derese4803 <derese.esubalew-c@oneacrefund.org>
2025-12-31 16:29:28 +0300

    Update models.py
    Signed-off-by: Derese4803 <derese.esubalew-c@oneacrefund.org>

--- models.py
+++ models.py
@@ -14,20 +14,18 @@ class BackCheck(Base):
     kebele = Column(String)
     tno_name = Column(String)
     checker_fa_name = Column(String)
-    cbe_acc = Column(String)          # Corrected Field
+    cbe_acc = Column(String)
     checker_phone = Column(String)    
     fenced = Column(String)           
 
-    # Guava (ዘይቶን)
+    # Species Data
     guava_beds = Column(Integer); guava_length = Column(Float); guava_sockets = Column(Integer); total_guava_sockets = Column(Integer)
-    
-    # Gesho (ጌሾ)
     gesho_beds = Column(Integer); gesho_length = Column(Float); gesho_sockets = Column(Integer); total_gesho_sockets = Column(Integer)
-
-    # Lemon (ሎሚ)
     lemon_beds = Column(Integer); lemon_length = Column(Float); lemon_sockets = Column(Integer); total_lemon_sockets = Column(Integer)
-
-    # Grevillea (ግራቪሊያ)
     grevillea_beds = Column(Integer); grevillea_length = Column(Float); grevillea_sockets = Column(Integer); total_grevillea_sockets = Column(Integer) 
+    
+    # Remarks / አስተያየቶች
+    auto_remark = Column(String)    # Automatic Over/Under status
+    general_remark = Column(String) # Manual user comments
     
     timestamp = Column(DateTime, default=datetime.datetime.utcnow)
