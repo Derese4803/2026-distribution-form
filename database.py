f7ebd94baf62c9b477b799f5e0d9b70b74a99960
Derese4803 <derese.esubalew-c@oneacrefund.org>
2025-12-31 12:01:39 +0300

    Update database.py
    Signed-off-by: Derese4803 <derese.esubalew-c@oneacrefund.org>

--- database.py
+++ database.py
@@ -1,9 +1,7 @@
-from sqlalchemy import create_all, create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import os
 
-# This creates a local database file named distribution.db
-DATABASE_URL = "sqlite:///./distribution.db"
+DATABASE_URL = "sqlite:///./distribution_2026.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
